bb5033448a5198a19fa9fdcdc92c8173e1a43934
Batzorig Tsergiinkhuu <bazoo@Batzorigs-MacBook-Pro.local>
2018-04-18 23:28:46 +0800

    edit

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/job',(req,res)=>{
     res.send(job);
   });
 })
-app.get('/jobDetail',(req,res)=>{
+app.get('/jobdetail',(req,res)=>{
   Job.findOne({ 'empId': 2 } ), function (err, job) {
     if (err) return handleError(err);
     console.log('length: '+ job);
@@ -114,83 +114,6 @@ app.get('/articles',(req,res)=>{
     res.send(article);
   });
 })
-
-app.post('/', function (req, res, next) {
-  // confirm that user typed same password twice
-  if (req.body.password !== req.body.passwordConf) {
-    var err = new Error('Passwords do not match.');
-    err.status = 400;
-    res.send("passwords dont match");
-    return next(err);
-  }
-
-  if (req.body.email &&
-    req.body.username &&
-    req.body.password &&
-    req.body.passwordConf) {
-
-    var userData = {
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-      passwordConf: req.body.passwordConf,
-    }
-
-    usr.create(userData, function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/profile');
-      }
-    });
-
-  } else if (req.body.logemail && req.body.logpassword) {
-    usr.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
-      if (error || !user) {
-        var err = new Error('Wrong email or password.');
-        err.status = 401;
-        return next(err);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/profile');
-      }
-    });
-  } else {
-    var err = new Error('All fields required.');
-    err.status = 400;
-    return next(err);
-  }
-})
-
-app.get('/profile', function (req, res, next) {
-  usr.findById(req.session.userId)
-    .exec(function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
-        if (user === null) {
-          var err = new Error('Not authorized! Go back!');
-          err.status = 400;
-          return next(err);
-        } else {
-          return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
-        }
-      }
-    });
-});
-app.get('/logout', function (req, res, next) {
-  if (req.session) {
-    // delete session object
-    req.session.destroy(function (err) {
-      if (err) {
-        return next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-  }
-});
 
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + '/index.html')
